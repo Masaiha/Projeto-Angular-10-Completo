06111fd36d397e9da9ff7b833e532510dfbd5e09
Masaiha <masanori.iha@gmail.com>
2020-08-10 14:36:13 -0300

    instalando ng-bootstrap

--- front-end/front-end/src/polyfills.ts
+++ front-end/front-end/src/polyfills.ts
@@ -57,7 +57,8 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
+/* Localize NG-Bootstrap */
+import '@angular/localize/init';
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
